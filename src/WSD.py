67ee442770766b4effcbd5da664f38be8a7fc3da
tzwel <39600182+tzwel@users.noreply.github.com>
2021-03-30 09:27:43 +0200

    split lists

--- src/WSD.py
+++ src/WSD.py
@@ -1,32 +1,8 @@
 ### SETUP
 import os
+from WSDservices import services
 from os import system
 system("title " + "WSD")
-
-services = [
-    "uhssvc",         # Microsoft Update Health Service
-    "wuauserv",       # Windows Update Service
-    "XboxGipSvc",     # Xbox Accessory Management Service
-    "XblGameSave",    # Xbox Live Game Save Service
-    "XblAuthManager", # Xbox Live Auth Manager
-    "XboxNetApiSvc",  # Xbox Live Networking Service
-    "wercplsupport",  # Problem Reports and Solutions
-    "WalletService",  # Wallet Service
-    "Fax",            # Fax
-    "WerSvc",         # Windows Error Reporting Service
-    "pla",            # Performance logs and alerts
-    "RstMwService",   # Windows store communication
-    "lfsvc",          # Geolocation Service
-    "diagnosticshub.standardcollector.service",
-                      # Microsoft® Diagnostics Hub Standard Collector
-    "wlidsvc",        # Microsoft Windows Live ID Service
-    "PNRPAutoReg",    # PNRP Machine Name Publication Service
-    "WMPNetworkSvc",  # Windows Media Player Network Sharing Service
-    "icssvc",         # Windows Mobile Hotspot Service
-    "DoSvc",          # Delivery Optimization service
-    "DiagTrack",      # Environment and telemetry
-    "DusmSvc"         # Data usage
-]
 
 ### FUNCTION DECLARATIONS
 def disable_services():
