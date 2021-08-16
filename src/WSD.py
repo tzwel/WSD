1022d654b1a7cf559163ab58ebde2e8d5d139ed2
tzwel <tzwel@int.pl>
2021-08-16 09:57:14 +0200

    add try catch blocks for 1st party services, add run.bat file

--- src/WSD.py
+++ src/WSD.py
@@ -8,6 +8,16 @@ system("mode con:cols=50 lines=20")
 ### FUNCTION DECLARATIONS
     # DISABLE WINDOWS SERVICES
 
+def success():
+    print("Done!")
+    print("Press any key to exit.")
+    os.system("pause >nul")
+
+def exception():
+    print("An exception has occurred.")
+    print("WSD has been stopped. Press any key to exit.")
+    os.system("pause >nul")
+
 #| windows services start
 def win_services():
     print("  ·   Enable / disable Windows services")
@@ -16,14 +26,24 @@ def win_services():
     userChoice = int(input(""))
 
     if userChoice == 1:
-        for i in services:
-            print("Trying to disable", i)
-            os.system(f'cmd /c "sc config {i} start= disabled"')
+        try:
+            for i in services:
+                print("Trying to disable", i)
+                os.system(f'cmd /c "sc config {i} start= disabled"')
+        except:
+            exception()
+        else:
+            success()
 
     elif userChoice == 2:
-        for i in services:
-            print("Trying to enable", i)
-            os.system(f'cmd /c "sc config {i} start= demand"')
+        try:
+            for i in services:
+                print("Trying to enable", i)
+                os.system(f'cmd /c "sc config {i} start= demand"')
+        except:
+            exception()
+        else:
+            success()
 
 #| windows services end
 #| 3p services start
@@ -62,6 +82,3 @@ if userChoice == 1:
     win_services()
 elif userChoice == 2:
     thirdparty_services()
-
-print("Done!")
-os.system("pause")
---
+++ src/run.bat
@@ -0,0 +1,1 @@
+start %CD%\WSD.py
